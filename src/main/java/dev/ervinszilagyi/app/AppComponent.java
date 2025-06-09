3879068dc4a7e6752204bde40f192b48e4f48f91
Ernyoke <ernyoke19930115@gmail.com>
2025-06-09 14:37:50 +0300

    Clean up AppComponent.

--- src/main/java/dev/ervinszilagyi/app/AppComponent.java
+++ src/main/java/dev/ervinszilagyi/app/AppComponent.java
@@ -3,26 +3,20 @@ package dev.ervinszilagyi.app;
 import dagger.BindsInstance;
 import dagger.Component;
 import dev.ervinszilagyi.ai.chatmodel.ChatModelProvider;
-import dev.ervinszilagyi.ai.chatmodel.ChatModelWithInfo;
-import dev.ervinszilagyi.ai.llmclient.LlmClient;
 import dev.ervinszilagyi.ai.llmclient.LlmClientProvider;
 import dev.ervinszilagyi.ai.mcpserver.McpLogMessageHandlerProvider;
 import dev.ervinszilagyi.ai.memory.ChatMemoryProvider;
 import dev.ervinszilagyi.commands.list.ListMcpDetails;
 import dev.ervinszilagyi.commands.session.ChatModelListenerProvider;
 import dev.ervinszilagyi.commands.session.ChatSession;
-import dev.ervinszilagyi.config.llm.LlmConfig;
 import dev.ervinszilagyi.config.llm.LlmConfigProvider;
 import dev.ervinszilagyi.config.mcp.McpConfigProvider;
-import dev.ervinszilagyi.md.StylizedPrinter;
 import dev.ervinszilagyi.terminal.TerminalModule;
-import dev.langchain4j.mcp.client.McpClient;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import org.jline.terminal.Terminal;
 
 import java.io.File;
-import java.util.Map;
 
 /**
  * Dagger component for the application, providing dependencies for the main application.
@@ -41,17 +35,7 @@ import java.util.Map;
 })
 @Singleton
 public interface AppComponent {
-    StylizedPrinter stylizedPrinter();
-
-    LlmConfig llmConfig();
-
     Terminal terminal();
-
-    ChatModelWithInfo chatModelWithInfo();
-
-    Map<String, McpClient> mcpClients();
-
-    LlmClient llmClient();
 
     ChatSession chatSession();
 
